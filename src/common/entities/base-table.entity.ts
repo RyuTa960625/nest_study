6bb394e0ae2c18d2f5f2251ae3ae2789213aaf7e
류태수 <r.tae.su.0625@gmail.com>
2024-10-31 04:21:37 +0900

    Feat: Serialization 적용

--- src/common/entities/base-table.entity.ts
+++ src/common/entities/base-table.entity.ts
@@ -1,13 +1,17 @@
+import { Exclude } from "class-transformer";
 import { CreateDateColumn, Entity, UpdateDateColumn, VersionColumn } from "typeorm";
 
 @Entity()
 export class BaseTable {
   @CreateDateColumn()
+  @Exclude()
   createdAt: Date;
     
   @UpdateDateColumn()
+  @Exclude()
   updatedAt: Date;
 
   @VersionColumn()
+  @Exclude()
   version: number;
 }
